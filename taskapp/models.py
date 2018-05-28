d522fea4e1aee5f4211cecce1035e9469822d4e9
sahasakkhar <sakkhar@mail.com>
2018-05-29 02:47:58 +0600

    add serializer

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=200, null=True, blank=True, verbose_name=("task_name"))
+    task_desc = models.TextField(null=True, blank=True, verbose_name=("task_desc"))
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task_name
+
